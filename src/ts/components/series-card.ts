a08b63790664129d33db8308e4ab6e20394d6192
Acrylic125 <acrylic125email@gmail.com>
2021-09-18 20:53:37 +0800

    SeriesCard components

--- src/ts/components/series-card.ts
+++ src/ts/components/series-card.ts
@@ -1,16 +1,36 @@
-import { Component, SerializableComponent } from "./component";
+import { Component } from "./component";
 
 export class SeriesCardColorStrip 
-        implements Component, SerializableComponent {
+        implements Component {
+
+    constructor(public color: string) {}
+
     toHTML(): string {
         return "";
     }
 
-    serialize(): any {
+}
 
+export class SeriesCardTitle
+        implements Component {
+
+    constructor(public text: string) {}
+
+    toHTML(): string {
+        return "";
     }
 
-    static deserialize(m: any): SeriesCardColorStrip {
-        return new SeriesCardColorStrip();
+}
+
+
+export class SeriesCard 
+        implements Component {
+    
+    constructor(public colorStrip: SeriesCardColorStrip, 
+                public title: SeriesCardTitle) {}
+
+    toHTML(): string {
+        return "";
     }
+
 }
