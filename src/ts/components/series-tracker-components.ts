fd84e479b2f1d1eba36bf5f9c1c4192e06b006a2
Acrylic125 <acrylic125email@gmail.com>
2021-09-30 10:24:05 +0800

    CSS class rename series-tracker-group -> series-tracker

--- src/ts/components/series-tracker-components.ts
+++ src/ts/components/series-tracker-components.ts
@@ -1,15 +1,15 @@
 
 /**
- *          <div class="series-tracker-group">
-              <div class="series-tracker-group__bg rel-0">
-                <div class="series-tracker-group__bg--circle"></div>
+ *          <div class="series-tracker">
+              <div class="series-tracker__bg rel-0">
+                <div class="series-tracker__bg--circle"></div>
               </div>
-              <article class="series-tracker-group__content">
+              <article class="series-tracker__content">
                 <header>Seasons</header>
                 <ol>
                   <li>
-                    <p class="series-tracker-group__content-item--title">Season 1</p>
-                    <p class="series-tracker-group__content-item--status">Episode 24</p>
+                    <p class="series-tracker__content-item--title">Season 1</p>
+                    <p class="series-tracker__content-item--status">Episode 24</p>
                   </li>
                 </ol>
               </article>
@@ -30,24 +30,24 @@ export interface ContentItem {
     callback?: ContentItemCallback
 }
 
-// <p class="series-tracker-group__content-item--title"> </p>
+// <p class="series-tracker__content-item--title"> </p>
 export function createSeriesTrackerContentItemTitle(title: string) {
     const titleElement = document.createElement('p');
     titleElement.innerText = title;
-    titleElement.classList.add('series-tracker-group__content-item--title');
+    titleElement.classList.add('series-tracker__content-item--title');
     return titleElement;
 }
 
-// <p class="series-tracker-group__content-item--status"> </p>
+// <p class="series-tracker__content-item--status"> </p>
 export function createSeriesTrackerContentItemStatus(status: string) {
     const titleElement = document.createElement('p');
     titleElement.innerText = status;
-    titleElement.classList.add('series-tracker-group__content-item--status');
+    titleElement.classList.add('series-tracker__content-item--status');
     return titleElement;
 }
 
 // <li>
-//   <p class="series-tracker-group__content-item--title"> </p>
+//   <p class="series-tracker__content-item--title"> </p>
 // </li>
 export function createSeriesTrackerContentItem(contentItem: ContentItem) {
     const contentItemElement = document.createElement('li');
@@ -67,7 +67,7 @@ export interface SeriesTrackerContent {
 export function createSeriesTrackerContent(seriesTrackerContent: SeriesTrackerContent) {
     const { title, contentItems } = seriesTrackerContent;
     const contentElement = document.createElement('article');
-    contentElement.classList.add('series-tracker-group__content');
+    contentElement.classList.add('series-tracker__content');
     if (title) 
         contentElement.appendChild(createSeriesTrackerTitle(title));
     const contentItemsElement = document.createElement('ul');
