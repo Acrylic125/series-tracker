5fb7e06f4d1c8ebc0d189f226d83db0e552f39cf
Acrylic125 <acrylic125email@gmail.com>
2021-10-03 09:42:17 +0800

    Moved rgb and hex functions out of utils to colors

--- src/ts/utils/colors.ts
+++ src/ts/utils/colors.ts
@@ -1,4 +1,16 @@
-import { randInt, rgbToHex } from "./utils";
+import { randInt } from "./utils";
+
+export const RED_HEX = 2 << 15;
+export const GREEN_HEX = 2 << 7;
+export const BLUE_HEX = 1;
+
+export function rgbToDec(r: number, g: number, b: number) {
+    return (r * RED_HEX) + (g * GREEN_HEX) + b; 
+}
+
+export function rgbToHex(r: number, g: number, b: number) {
+    return rgbToDec(r, g, b).toString(16);
+}
 
 /**
  * These options act as constants for 2 components of the
@@ -29,6 +41,30 @@ export const DARK_DEFAULT: ColorOption = {
     alphaConstant: 184,
     betaConstant: 26
 };
+
+export interface RGB {
+    red: number
+    green: number
+    blue: number
+}
+
+export interface Color {
+    rgb: RGB
+    toHex(): string
+    toPrefixedHex(): string
+    toDecimal(): number
+    brighten(percent: number): void
+    darken(percent: number): void
+}
+
+export function newColor(rgb: RGB): Color {
+    return {
+        rgb,
+        toHex() {
+            return rgbToHex()
+        }
+    }
+}
 
 export function randColor(color: ColorOption, caseNumber = randInt(1, 6)) {
     const variant = randInt(0, 255);
--- src/ts/utils/utils.ts
+++ src/ts/utils/utils.ts
@@ -1,21 +1,9 @@
-import fs, { mkdir } from 'fs';
+import fs from 'fs';
 import { sep } from 'path';
 import { Stream } from 'stream';
 
 export function randInt(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-export const RED_HEX = 2 << 15;
-export const GREEN_HEX = 2 << 7;
-export const BLUE_HEX = 1;
-
-export function rgbToDec(r: number, g: number, b: number) {
-    return (r * RED_HEX) + (g * GREEN_HEX) + b; 
-}
-
-export function rgbToHex(r: number, g: number, b: number) {
-    return rgbToDec(r, g, b).toString(16);
 }
 
 export function deducePath(path: string) {
