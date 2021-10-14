9fc1443858418e4f51aadf05b4088aeceae3b35e
Acrylic125 <acrylic125email@gmail.com>
2021-10-14 17:03:28 +0800

    Shifting and swapping array utilities

--- src/ts/utils/utils.ts
+++ src/ts/utils/utils.ts
@@ -93,3 +93,70 @@ export function removeElementFromArray<T>(array: T[], element: T) {
     }
     return false;
 }
+
+export function isIndexOutOfBoundsOfArray<T>(array: T[], index: number) {
+    return index < 0 || index >= array.length;
+}
+
+export function swapElementInArray<T>(array: T[], swapFromIndex: number, swapToIndex: number) {
+    if (swapFromIndex === swapToIndex || isIndexOutOfBoundsOfArray(array, swapFromIndex) || isIndexOutOfBoundsOfArray(array, swapToIndex))
+        return;
+    const itemFrom = array[swapFromIndex],
+          itemTo = array[swapToIndex];
+    array[swapFromIndex] = itemTo;
+    array[swapToIndex] = itemFrom;
+}
+
+/**
+ * Shifting Left <----
+ * Example:
+ * array = ['a', 'b', 'c', 'd', 'e']
+ * If the 3rd index element is shifted left then
+ * array -> ['a', 'c', 'b', 'd', 'e']
+ * 
+ * If wrap is true, then if 0th index element is shifted, 
+ * it will become the last element. Otherwise, it will not shift.
+ */
+export function shifElementtLeftByIndex<T>(array: T[], index: number, wrap = true) {
+    var swapIndex = index - 1;
+    if (index === 0) {
+        if (!wrap)
+            return;
+        swapIndex = array.length - 1;
+    }
+    swapElementInArray(array, index, swapIndex);
+}
+
+export function shifElementtLeft<T>(array: T[], element: T, wrap = true) {
+    const index = array.indexOf(element);
+    if (index <= -1)
+        return;
+    shifElementtLeftByIndex(array, index, wrap);
+}
+
+/**
+ * Shifting Right ---->
+ * Example:
+ * array = ['a', 'b', 'c', 'd', 'e']
+ * If the 3rd index element is shifted right then
+ * array -> ['a', 'b', 'd', 'c', 'e']
+ * 
+ * If wrap is true, then if last element is shifted, 
+ * it will become the 0th element. Otherwise, it will not shift.
+ */
+ export function shifElementtRightByIndex<T>(array: T[], index: number, wrap = true) {
+    var swapIndex = index + 1;
+    if (index === array.length) {
+        if (!wrap)
+            return;
+        swapIndex = 0;
+    }
+    swapElementInArray(array, index, swapIndex);
+}
+
+export function shifElementtRight<T>(array: T[], element: T, wrap = true) {
+    const index = array.indexOf(element);
+    if (index <= -1)
+        return;
+    shifElementtRightByIndex(array, index, wrap);
+}
